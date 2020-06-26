5448aa22dcc3e5e4f8352fbb213b2b2a3007c94f
Chalanika <chalanikasewwandi@gmail.com>
2020-06-26 21:50:37 +0530

    created services provides view ui

--- web/src/components/booking/serviceProviders.jsx
+++ web/src/components/booking/serviceProviders.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "../shared/header";
-import { Row, Card, CardHeader, CardBody, Container } from "reactstrap";
+import { Row, Card, CardHeader, CardBody, CardImg,CardTitle,CardText,Container ,Button} from "reactstrap";
 
 class serviceProviders extends Component {
   state = {
@@ -18,12 +18,108 @@ class serviceProviders extends Component {
             <div className=" col">
               <Card className=" shadow">
                 <CardHeader className=" bg-transparent">
-                  <h3 className=" mb-0">Service Providers</h3>
+                  <h3 className=" mb-0" style={{fontSize:40}}>Service Providers</h3>
                 </CardHeader>
                 <CardBody>
                   <div style={{ minHeight: 400 }}>
-                    {/* Page Content */}    
-                   
+                    {/* Page Content */} 
+                    <b style={{fontSize:30, marginBottom:0}}>Service Centers</b>
+                    <div className="m-5 mt-0">
+                        <Row>
+                        <Card style={{ width: "18rem" ,marginLeft:9, marginRight:5}}>
+                            <CardImg
+                            alt="..."
+                            style={{ height:150 }}
+                            src={require("assets/images/sp/laugfsCarCare.jpg")}
+                            top
+                            />
+                            <CardBody>
+                            <CardTitle><a href="">LAUGFS CarCare</a></CardTitle>
+
+                            {/* <CardText>
+                                Some quick example text to build on the card title and make up
+                                the bulk of the card's content.
+                            </CardText> */}
+                            {/* <Button
+                                color="primary"
+                                href="#pablo"
+                                onClick={e => e.preventDefault()}
+                            >
+                                Go somewhere
+                            </Button> */}
+                            </CardBody>
+                        </Card>  
+                        <Card style={{ width: "18rem" , marginRight:5}}>
+                            <CardImg
+                            alt="..."
+                            style={{ height:150 }}
+                            src={require("assets/images/sp/Micro.jpg")}
+                            top
+                            />
+                            <CardBody>
+                            <CardTitle><a href="">Micro CarCare</a></CardTitle>
+                            
+                            {/* <CardText>
+                                Some quick example text to build on the card title and make up
+                                the bulk of the card's content.
+                            </CardText> */}
+                            {/* <Button
+                                color="primary"
+                                href="#pablo"
+                                onClick={e => e.preventDefault()}
+                            >
+                                Go somewhere
+                            </Button> */}
+                            </CardBody>
+                        </Card>  
+                        <Card style={{ width: "18rem", marginRight:5 }}>
+                            <CardImg
+                            alt="..."
+                            style={{ height:150 }}
+                            src={require("assets/images/sp/automiraj.jpg")}
+                            top
+                            />
+                            <CardBody>
+                            <CardTitle><a href="">Auto Miraj</a></CardTitle>
+                            
+                            {/* <CardText>
+                                Some quick example text to build on the card title and make up
+                                the bulk of the card's content.
+                            </CardText> */}
+                            {/* <Button
+                                color="primary"
+                                href="#pablo"
+                                onClick={e => e.preventDefault()}
+                            >
+                                Go somewhere
+                            </Button> */}
+                            </CardBody>
+                        </Card>   
+                        <Card style={{ width: "18rem", marginRight:5 }}>
+                            <CardImg
+                            alt="..."
+                            style={{ height:150 }}
+                            src={require("assets/images/sp/amw.jpg")}
+                            top
+                            />
+                            <CardBody>
+                            <CardTitle><a href="">AMW Service Center</a></CardTitle>
+                            
+                            {/* <CardText>
+                                Some quick example text to build on the card title and make up
+                                the bulk of the card's content.
+                            </CardText> */}
+                            {/* <Button
+                                color="primary"
+                                href="#pablo"
+                                onClick={e => e.preventDefault()}
+                            >
+                                Go somewhere
+                            </Button> */}
+                            </CardBody>
+                        </Card>   
+                        </Row>
+                    </div>   
                   </div>
                 </CardBody>
               </Card>
