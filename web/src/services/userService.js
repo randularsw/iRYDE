55faf6ee2c58ff14e1068239845587cecf35c485
Chalanika <chalanikasewwandi@gmail.com>
2020-06-28 15:04:06 +0530

    created serviceProviders list manualy in userService.js

---
+++ web/src/services/userService.js
@@ -0,0 +1,67 @@
+const serviceProviders = [
+    {
+        _id: "5b21ca3eeb7f6fbccd471815",
+        name:"LAUGFS CarCare",
+        email:"LAUGFSCarCare@gmail.com",
+        address :"25/p,pannipitya",
+        profileImage:"assets/images/sp/laugfsCarCare.jpg",
+        contact:5556555458,
+        ratings:[
+            {_id="1332256",rate:3,review="fgtgygyuhuuuuhu"},
+            {_id="1332255",rate:3,review="fgtgygyuhuuuuhu"},
+        ],
+        services:[
+            {_id="1332256",title="oil change",description:"hghdg hsgysgy hgaygyst",image:"assets/images/sp/laugfsCarCare.jpg"},
+            {_id="1332255",title="oil change",description:"dyhujdidokjdi",image:"assets/images/sp/laugfsCarCare.jpg"},
+        ],
+      },
+      {
+        _id: "5b21ca3eeb7f6fbccd471816",
+        name:"Micro CarCare",
+        email:"Micro CarCare",
+        address :"544/jhjhjhjhj",
+        profileImage:"assets/images/sp/Micro.jpg",
+        contact:55565,
+        ratings:[
+            {_id="1332256",rate:3,review="fgtgygyuhuuuuhu"},
+            {_id="1332255",rate:3,review="fgtgygyuhuuuuhu"},
+        ],
+        services:[
+            {_id="1332256",title="oil change",description:"hghdg hsgysgy hgaygyst",image:"assets/images/sp/laugfsCarCare.jpg"},
+            {_id="1332255",title="oil change",description:"dyhujdidokjdi",image:"assets/images/sp/Micro.jpg"},
+        ],
+      },
+      {
+        _id: "5b21ca3eeb7f6fbccd471817",
+        name:"Auto Miraj",
+        email:"Auto Miraj",
+        address :"Auto Miraj",
+        profileImage:"assets/images/sp/automiraj.jpg",
+        contact:55565,
+        ratings:[
+            {_id="1332256",rate:3,review="fgtgygyuhuuuuhu"},
+            {_id="1332255",rate:3,review="fgtgygyuhuuuuhu"},
+        ],
+        services:[
+            {_id="1332256",title="oil change",description:"hghdg hsgysgy hgaygyst",image:"assets/images/sp/laugfsCarCare.jpg"},
+            {_id="1332255",title="oil change",description:"dyhujdidokjdi",image:"assets/images/sp/Micro.jpg"},
+        ],
+      },
+      {
+        _id: "5b21ca3eeb7f6fbccd471818",
+        name:"AMW Service Center",
+        email:"AMW Service Center",
+        address :"AMW Service Center",
+        profileImage:"assets/images/sp/amw.jpg",
+        contact:55565,
+        ratings:[
+            {_id="1332256",rate:3,review="fgtgygyuhuuuuhu"},
+            {_id="1332255",rate:3,review="fgtgygyuhuuuuhu"},
+        ],
+        services:[
+            {_id="1332256",title="oil change",description:"hghdg hsgysgy hgaygyst",image:"assets/images/sp/laugfsCarCare.jpg"},
+            {_id="1332255",title="oil change",description:"dyhujdidokjdi",image:"assets/images/sp/automiraj.jpg"},
+        ],
+      },
+];
+
