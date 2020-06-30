9115ce869ed6f86239dc5329c5839cc04dc9d491
nawanjana <nkeshalya@gmail.com>
2020-06-30 19:00:35 +0530

    issues fixed and added some other endpoints

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -5,12 +5,12 @@ router.route("/add").post((req, res) => {
   const servicename = req.body.servicename;
   const description = req.body.description;
 
-  const newServices = new Service({
+  const newService = new Service({
     servicename,
     description,
   });
-
-  newServices
+  console.log();
+  newService
     .save()
     .then(() => res.json("Service added"))
     .catch((err) => res.status(400).json("Error:" + err));
@@ -19,6 +19,18 @@ router.route("/add").post((req, res) => {
 router.route("/").get((req, res) => {
   Service.find()
     .then((services) => res.json(services))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Service.findById(req.params.id)
+    .then((services) => res.json(services))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Service.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Service deleted"))
     .catch((err) => res.status(400).json("Error:" + err));
 });
 
