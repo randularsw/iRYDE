2cb2d085ab237a28fd3ce29b8b89d2f46593957e
nawanjana <nkeshalya@gmail.com>
2020-07-03 09:49:09 +0530

    changed the class component to a functional component

--- web/src/components/servicelist/serviceTableRow.jsx
+++ web/src/components/servicelist/serviceTableRow.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import {
   DropdownMenu,
@@ -6,52 +6,40 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
 } from "reactstrap";
-import Axios from "axios";
 
-class ServiceTableRow extends Component {
-  constructor(props) {
-    super(props);
-    this.deleteStudent = this.deleteStudent.bind(this);
-  }
+const ServiceTableRow = (props) => {
+  return (
+    <tr>
+      <td>{props.obj.servicename}</td>
 
-  deleteStudent() {
-    Axios.delete("http://localhost:4000/services/" + this.props.obj._id)
-      .then((res) => {
-        console.log("Service deleted successfully");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  render() {
-    return (
-      <tr>
-        <td>{this.props.obj.servicename}</td>
+      <td>{props.obj.description}</td>
 
-        <td>{this.props.obj.description}</td>
-
-        <td className="text-right">
-          <UncontrolledDropdown>
-            <DropdownToggle
-              className="btn-icon-only text-light"
+      <td className="text-right">
+        <UncontrolledDropdown>
+          <DropdownToggle
+            className="btn-icon-only text-light"
+            href="#pablo"
+            role="button"
+            size="sm"
+            color=""
+            onClick={(e) => e.preventDefault()}
+          >
+            <i className="fas fa-ellipsis-v" />
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-arrow" right>
+            <DropdownItem
               href="#pablo"
-              role="button"
-              size="sm"
-              color=""
-              onClick={(e) => e.preventDefault()}
+              onClick={() => {
+                props.deleteService(props.obj._id);
+              }}
             >
-              <i className="fas fa-ellipsis-v" />
-            </DropdownToggle>
-            <DropdownMenu className="dropdown-menu-arrow" right>
-              <DropdownItem href="#pablo" onClick={this.deleteStudent}>
-                Delete
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </td>
-      </tr>
-    );
-  }
-}
+              Delete
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </td>
+    </tr>
+  );
+};
 
 export default ServiceTableRow;
