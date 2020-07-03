d914fe9027908c4eb2d64836e9ce8e7f3372849b
Chalanika <chalanikasewwandi@gmail.com>
2020-07-03 22:05:23 +0530

    create route to add vehicles

---
+++ backend/routes/vehicles.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const Vehicle = require('../models/vehicle');
+
+router.post('/', async (req,res) =>{
+    console.log(req.body);
+    const newVehicle = new Vehicle({
+        brand:req.body.brand,
+        model:req.body.model,
+        type:req.body.type,
+        vehicleNo:req.body.vehicleNo,
+        ownerId:req.body.vehicleNo,
+    })
+    try {
+        const saved = await newVehicle.save();
+        res.json('vehicle added successfully');    
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
