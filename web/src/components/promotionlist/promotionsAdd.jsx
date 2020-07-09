64c885d418e55d92841cf7c0fe12e34244fa21ae
nawanjana <nkeshalya@gmail.com>
2020-07-09 19:41:48 +0530

    changed some dimensions of the form

--- web/src/components/promotionlist/promotionsAdd.jsx
+++ web/src/components/promotionlist/promotionsAdd.jsx
@@ -57,114 +57,116 @@ class PromotionsAdd extends Component {
         <Header />
         <Container className=" mt--9" fluid>
           {/* Table */}
-          <Row>
-            <div className=" col">
-              <Card className=" shadow" style={{ backgroundColor: "#f4f5f7" }}>
-                <CardHeader className=" bg-transparent">
-                  <h3 className=" mb-0">Add promotions</h3>
-                </CardHeader>
-                <CardBody>
-                  <div style={{ minHeight: 400 }}>
-                    {/* Page Content */}
 
-                    <Form onSubmit={this.onSubmit}>
-                      <Row
-                        className="justify-content-md-center"
-                        style={{ marginTop: ".5rem" }}
-                      >
-                        <Col md="8">
-                          <FormGroup>
-                            <Input
-                              className="form-control-alternative"
-                              placeholder="Title"
-                              type="text"
-                              required={true}
-                              value={this.state.title}
-                              onChange={this.onChangeTitle}
+          <Col lg="10">
+            <Card
+              className="bg-secondary shadow border-0 mb-7"
+              style={({ backgroundColor: "#f4f5f7" }, { marginLeft: "7.8rem" })}
+            >
+              <CardHeader className=" bg-transparent">
+                <h3 className=" mb-0">Add promotions</h3>
+              </CardHeader>
+              <CardBody>
+                <div style={{ minHeight: 400 }}>
+                  {/* Page Content */}
+
+                  <Form onSubmit={this.onSubmit}>
+                    <Row
+                      className="justify-content-md-center"
+                      style={{ marginTop: ".5rem" }}
+                    >
+                      <Col md="8">
+                        <FormGroup>
+                          <Input
+                            className="form-control-alternative"
+                            placeholder="Title"
+                            type="text"
+                            required={true}
+                            value={this.state.title}
+                            onChange={this.onChangeTitle}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row
+                      className="justify-content-md-center"
+                      style={{ marginTop: "1.5rem" }}
+                    >
+                      <Col md="8">
+                        <FormGroup>
+                          <Input
+                            className="form-control-alternative"
+                            placeholder="Enter the description"
+                            type="textarea"
+                            rows="2"
+                            required={true}
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+
+                    <Row
+                      className="justify-content-md-center"
+                      style={{ marginTop: "1.5rem" }}
+                    >
+                      <Col md="4">
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <DatePicker
+                              onChange={this.onChangeStartDate}
+                              selected={this.state.startDate}
+                              minDate={minDate}
+                              placeholderText="From date"
+                              inputProps={{
+                                placeholder: "Date Picker Here",
+                              }}
+                              timeFormat={true}
                             />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row
-                        className="justify-content-md-center"
-                        style={{ marginTop: "1.5rem" }}
-                      >
-                        <Col md="8">
-                          <FormGroup>
-                            <Input
-                              className="form-control-alternative"
-                              placeholder="Enter the description"
-                              type="textarea"
-                              rows="2"
-                              required={true}
-                              value={this.state.description}
-                              onChange={this.onChangeDescription}
+                          </InputGroup>
+                        </FormGroup>
+                      </Col>
+
+                      <Col md="4">
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <DatePicker
+                              onChange={this.onChangeEndDate}
+                              selected={this.state.endDate}
+                              minDate={this.state.startDate}
+                              placeholderText="To date"
+                              inputProps={{
+                                placeholder: "Date Picker Here",
+                              }}
+                              timeFormat={false}
                             />
-                          </FormGroup>
-                        </Col>
-                      </Row>
+                          </InputGroup>
+                        </FormGroup>
+                      </Col>
+                    </Row>
 
-                      <Row
-                        className="justify-content-md-center"
-                        style={{ marginTop: "1.5rem" }}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Button
+                        color="primary"
+                        type="submit"
+                        size="lg"
+                        style={{ marginTop: "2.5rem", width: "150px" }}
                       >
-                        <Col md="4">
-                          <FormGroup>
-                            <InputGroup className="input-group-alternative">
-                              <DatePicker
-                                onChange={this.onChangeStartDate}
-                                selected={this.state.startDate}
-                                minDate={minDate}
-                                placeholderText="From date"
-                                inputProps={{
-                                  placeholder: "Date Picker Here",
-                                }}
-                                timeFormat={true}
-                              />
-                            </InputGroup>
-                          </FormGroup>
-                        </Col>
-
-                        <Col md="4">
-                          <FormGroup>
-                            <InputGroup className="input-group-alternative">
-                              <DatePicker
-                                onChange={this.onChangeEndDate}
-                                selected={this.state.endDate}
-                                minDate={this.state.startDate}
-                                placeholderText="To date"
-                                inputProps={{
-                                  placeholder: "Date Picker Here",
-                                }}
-                                timeFormat={false}
-                              />
-                            </InputGroup>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "center",
-                          alignItems: "center",
-                        }}
-                      >
-                        <Button
-                          color="primary"
-                          type="submit"
-                          size="lg"
-                          style={{ marginTop: "2.5rem", width: "150px" }}
-                        >
-                          Add
-                        </Button>
-                      </div>
-                    </Form>
-                  </div>
-                </CardBody>
-              </Card>
-            </div>
-          </Row>
+                        Add
+                      </Button>
+                    </div>
+                  </Form>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
         </Container>
       </>
     );
