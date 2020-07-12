619e4bbcc6f6424172218634a8c900e67f300d03
Chalanika <chalanikasewwandi@gmail.com>
2020-07-12 17:52:44 +0530

    get data from backend to view

--- web/src/components/booking/spBookingView.jsx
+++ web/src/components/booking/spBookingView.jsx
@@ -3,15 +3,32 @@ import Header from "components/shared/header";
 import { Container, Row, CardHeader, CardBody, Button, Col } from "reactstrap";
 import { Card } from "@material-ui/core";
 import { UserContext } from "core/userContext";
+import { getIncomingAppointments } from "services/bookingService";
 
 class SpBookingView extends Component {
   static contextType = UserContext;
-  state={
+  state = {
+    currentUserId: "",
+    incoming: [],
+  };
 
+  async componentDidMount() {
+    try {
+      const userData = await this.context.currentUser();
+      this.state.currentUserId = userData.user?._id;
+      const { data: incoming } = await getIncomingAppointments(
+        userData.user?._id
+      );
+      this.setState({ incoming });
+    } catch (error) {
+      console.log("err", error);
+    }
   }
 
-  async componentDidMount(){
-    
+  changeDateFormat(f) {
+    let d = new Date(f); 
+    let x = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()
+    return (x);
   }
 
   render() {
@@ -32,60 +49,75 @@ class SpBookingView extends Component {
                     <div className="">
                       <h3 className="text-gray">Incoming</h3>
                       <div className="ml-8 mt-4">
-                        <Card className="ml-30 mr-30" style={{ width: 800 }}>
-                          <CardHeader className="m-0 p-1  pt-2 bg-info">
-                            <Row>
-                              <div className="col-7">
-                                <i className="fas fa-user pr-2 pl-2"></i>Hashan
-                              </div>
-                              <div className="col">
-                                <Row>
-                                  <small className="mr-3 mt-1">
-                                    <b> Date:</b>2020/08/19{" "}
+                        {this.state.incoming.map((i) => (
+                          <Card
+                            className="ml-30 mr-30 mb-3"
+                            style={{ width: 800 }}
+                            key={i._id}
+                          >
+                            <CardHeader className="m-0 p-1  pt-2 bg-info" >
+                              <Row>
+                                <div className="col-7">
+                                  <i className="fas fa-user pr-2 pl-2"></i>
+                                  {i.voName}
+                                </div>
+                                <div className="col">
+                                  <Row>
+                                    <small className="mr-3 mt-1">
+                                      <b> Date:</b>
+                                      {this.changeDateFormat(i?.date)}
+                                    </small>
+                                    <small className="mr-3 mt-1">
+                                      <b> Time:</b>{i?.time}
+                                    </small>
+                                    <div>
+                                      <Button size="sm" color="default">Accept</Button>
+                                    </div>
+                                  </Row>
+                                </div>
+                              </Row>
+                            </CardHeader>
+                            <CardBody className="m-0 p-0">
+                              <Row className="m-0 p-0">
+                                <div className="col-7 border-right">
+                                  <small>
+                                    <b>Services</b>
                                   </small>
-                                  <small className="mr-3 mt-1">
-                                    <b> Time:</b> 8.00 AM
-                                  </small>
-                                  <div>
-                                    <Button size="sm">Accept</Button>
-                                  </div>
-                                </Row>
-                              </div>
-                            </Row>
-                          </CardHeader>
-                          <CardBody className="m-0 p-0">
-                            <Row className="m-0 p-0">
-                              <div className="col-7 border-right">
-                                <small >
-                                  <b>Services</b>
-                                </small>
-                                <ul>
-                                  <li><small>Lorem ipsum</small></li>
-                                  <li><small>Lorem ipsum</small></li>
+                                  <ul>
+                                   {i?.services.map((s)=>(
+                                    <li key={s}> 
+                                      <small>{s}</small>
+                                    </li>
+                                   ))}
+                                    
+                                  </ul>
+                                </div>
+                                {i.vehicle.map((v)=>(
+                                     <div className="col mt-2 mb-2" key={v._id}>
                                   
-                                </ul>
-                              </div>
-                              <div className="col mt-2">
-                                <Row>
-                                <small className='mr-3 ml-3 mt-2'>
-                                  <b>Vehicle No:</b>WD 4578
-                                </small>
-                                <small className='mr-3 ml-2 mt-2'>
-                                  <b>Vehicle Type:</b>Car
-                                </small>
-                                </Row>
-                               <Row>
-                               <small className='mr-3 ml-3 mt-2' >
-                                  <b>Vehicle Brand:</b>Toyota
-                                </small>
-                                <small className='mr-3 ml-2 mt-2'>
-                                  <b>Vehicle Model:</b>Vitz
-                                </small>
-                               </Row>
-                              </div>
-                            </Row>
-                          </CardBody>
-                        </Card>
+                                     <Row>
+                                       <small className="mr-3 ml-3 mt-2">
+                                         <b>Vehicle No:</b>{v.vehicleNo}
+                                       </small>
+                                       <small className="mr-3 ml-2 mt-2">
+                                         <b>Vehicle Type:</b>{v.type}
+                                       </small>
+                                     </Row>
+                                     <Row>
+                                       <small className="mr-3 ml-3 mt-2">
+                                         <b>Vehicle Brand:</b>{v.brand}
+                                       </small>
+                                       <small className="mr-3 ml-2 mt-2">
+                                         <b>Vehicle Model:</b>{v.model}
+                                       </small>
+                                     </Row>
+                                   </div>
+                                    ))}
+                               
+                              </Row>
+                            </CardBody>
+                          </Card>
+                        ))}
                       </div>
                     </div>
                   </div>
--- web/src/services/bookingService.js
+++ web/src/services/bookingService.js
@@ -5,3 +5,8 @@ export function addBooking(data) {
   console.log("booking", data);
   return axios.post("http://localhost:4000/bookings", data);
 }
+//specific sp incoming appointments
+export function getIncomingAppointments(id){
+  console.log(id);
+  return axios.get("http://localhost:4000/bookings/incoming/" + id);
+}
