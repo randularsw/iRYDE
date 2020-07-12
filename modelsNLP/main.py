69f1ba3b7420d40558de2441c47b1e9dc0689f2b
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-12 17:33:50 +0530

    usage of nltk corpus stopwords

--- modelsNLP/main.py
+++ modelsNLP/main.py
@@ -5,10 +5,10 @@
 
 import string
 from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords   #this has all stopwords in many language
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
-
-
-
 
 #read text
 text = open('read.txt', encoding='utf=8').read()
@@ -28,30 +28,31 @@ clean_text = lower_case.translate(str.maketrans('','',string.punctuation))
     #
     #Returns : Returns the translation table which specifies the conversions that can be used
 
+#2 - tokenization
+#how to break up the sentence to words
 
-
-
-tokenized_words = clean_text.split() 
-
-
-
+#tokenized_words = clean_text.split()
+#split() takes lot of time to analyze
+#split() can be done using nltk
+tokenized_words = word_tokenize(clean_text,'english')
 
 #3 - stop words
 #words that does not add any meaning to the sentence
 
+#instead of using stop_words, we can use nltk.corpus (its a dataset that nltk has)
+#from that import stopwords (a list of stop_words inside of corpus dataset)
 
 
-
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+# stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+#               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+#               "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+#               "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+#               "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+#               "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+#               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+#               "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+#               "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+#               "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 
 
@@ -61,7 +62,6 @@ final_words = []
 for word in tokenized_words:
     if word not in stopwords.words('english'):
         final_words.append(word)
-
 
 #5 - NPL Emotion Algorithm
     #1 - check if the word in the final word list is also present in emotion.txt
