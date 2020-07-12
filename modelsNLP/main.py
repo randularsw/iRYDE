9a0836fd5abd9bbf1a832535f7b0c002fa38440c
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-12 17:45:13 +0530

    sentiment analysis report generated

--- modelsNLP/main.py
+++ modelsNLP/main.py
@@ -102,3 +102,9 @@ def sentiment_analyse(sentiment_text):
 
 
 sentiment_analyse(clean_text)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
