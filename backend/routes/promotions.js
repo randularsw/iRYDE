fc7528c7bceb418b0926d27e745056dd85d75bc0
nawanjana <nkeshalya@gmail.com>
2020-07-15 17:09:15 +0530

    routes modified in promotions

--- backend/routes/promotions.js
+++ backend/routes/promotions.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 let Promotion = require("../models/promotions");
 
 router.route("/add").post((req, res) => {
+  const ownerId = req.body.ownerId;
   const title = req.body.title;
   const description = req.body.description;
   const startDate = Date.parse(req.body.startDate);
   const endDate = Date.parse(req.body.endDate);
 
   const newPromotion = new Promotion({
+    ownerId,
     title,
     description,
     startDate,
@@ -20,8 +22,9 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.route("/").get((req, res) => {
-  Promotion.find()
+router.route("/:id").get((req, res) => {
+  console.log(req.params.id);
+  Promotion.find({ ownerId: req.params.id })
     .then((promotions) => res.json(promotions))
     .catch((err) => res.status(400).json("Error:" + err));
 });
