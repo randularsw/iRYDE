e13a8af37b6c06cadf842b40a199c7eb54f0e702
nawanjana <nkeshalya@gmail.com>
2020-07-17 16:56:59 +0530

    added axios delete function

--- web/src/services/promotionService.js
+++ web/src/services/promotionService.js
@@ -8,3 +8,7 @@ export function getPromotions(id) {
 export function addPromotions(data) {
   return axios.post("http://localhost:4000/promotions/add", data);
 }
+
+export function deletePromotions(id) {
+  return axios.delete("http://localhost:4000/promotions/" + id);
+}
