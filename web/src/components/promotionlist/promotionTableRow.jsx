1fb3ca3bef07ddf510dc6530c549a4c1219a17e7
nawanjana <nkeshalya@gmail.com>
2020-07-17 15:55:08 +0530

    Updated ui for editing and deleting

--- web/src/components/promotionlist/promotionTableRow.jsx
+++ web/src/components/promotionlist/promotionTableRow.jsx
@@ -15,6 +15,28 @@ const PromotionTableRow = (props) => {
       <td>{props.obj.description}</td>
       <td>{props.obj.startDate.substring(0, 10)}</td>
       <td>{props.obj.endDate.substring(0, 10)}</td>
+      <td className="text-right">
+        <UncontrolledDropdown>
+          <DropdownToggle
+            className="btn-icon-only text-light"
+            href="#pablo"
+            role="button"
+            size="sm"
+            color=""
+            onClick={(e) => e.preventDefault()}
+          >
+            <i className="fas fa-ellipsis-v" />
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-arrow" right>
+            <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+              Delete
+            </DropdownItem>
+            <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+              Edit
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </td>
     </tr>
   );
 };
