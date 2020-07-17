541e1484a975e323ba24e9b9c61ad8d55921a4c3
Chalanika <chalanikasewwandi@gmail.com>
2020-07-17 15:22:57 +0530

    created updated timeslot function

--- web/src/services/timeSlots.js
+++ web/src/services/timeSlots.js
@@ -3,3 +3,8 @@ import axios from "axios";
 export function getTimeSlots(id,date) {
   return axios.get("http://localhost:4000/timeslots/"+id+'/'+date);
 }
+
+export function updateTimeSLot(id,data){
+  console.log(4444,data);
+  return axios.put('http://localhost:4000/timeslots/'+id,data);
+}
