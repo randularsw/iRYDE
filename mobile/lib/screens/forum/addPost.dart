a005c424c8a9b283f504e36ae1234344deb6e52d
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-03 20:20:44 +0530

    addPost page created

--- mobile/lib/screens/forum/addPost.dart
+++ mobile/lib/screens/forum/addPost.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iRYDE/components/myRaisedButton.dart';
 
 class AddPost extends StatefulWidget {
   static const String id = 'add_post';
@@ -8,6 +9,26 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  String _postText;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildPostText() {
+    return Container(
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'What\'s on your mind..'),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Post is empty!';
+          }
+        },
+        onSaved: (String value) {
+          _postText = value;
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +38,55 @@ class _AddPostState extends State<AddPost> {
           child: Text('Add Post'),
           margin: EdgeInsets.only(right: 58.0),
         )),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(12),
+        child: Form(
+            key: _formKey,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _buildPostText(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      MyRaisedButton(
+                        onPressed: () {
+                          if (!_formKey.currentState.validate()) {
+                            return;
+                          }
+                          _formKey.currentState.save();
+                          print(_postText);
+                        },
+                        buttonText: 'Preview',
+                        formKey: _formKey,
+                        questionText: _postText,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      MyRaisedButton(
+                        formKey: _formKey,
+                        questionText: _postText,
+                        buttonText: 'Post',
+                        onPressed: () {
+                          if (!_formKey.currentState.validate()) {
+                            return;
+                          }
+                          _formKey.currentState.save();
+                          print(_postText);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
