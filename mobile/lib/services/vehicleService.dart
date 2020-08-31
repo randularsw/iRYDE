f8b3a89b8eedca1e746dda71974f838931208a20
chalanika <chalanikasewwandi@gmail.com>
2020-08-31 20:39:32 +0530

    Get vehicles of specific user

---
+++ mobile/lib/services/vehicleService.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class VehicleService{
+  
+  Future<List> getVehicles(userId) async {
+    final res = await http.get('http://192.168.1.102:4000/vehicles/vo/'+ userId);
+    List data = jsonDecode(res.body);
+    return data;
+  }
+}
