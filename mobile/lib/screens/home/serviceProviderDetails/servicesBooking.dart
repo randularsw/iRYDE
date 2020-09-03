968f3c98ebff224c43c9f6b3b365f3d713366e50
chalanika <chalanikasewwandi@gmail.com>
2020-09-03 14:50:57 +0530

    added date picker

--- mobile/lib/screens/home/serviceProviderDetails/servicesBooking.dart
+++ mobile/lib/screens/home/serviceProviderDetails/servicesBooking.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iRYDE/components/drawer.dart';
 import 'package:iRYDE/services/servicesService.dart';
 import 'package:iRYDE/services/vehicleService.dart';
 
 class ServiceBooking extends StatefulWidget {
   static const String id = 'services_booking';
+  // String something;
+  // ServiceBooking(this.something);
   @override
   _ServiceBookingState createState() => _ServiceBookingState();
 }
@@ -15,11 +18,13 @@ class _ServiceBookingState extends State<ServiceBooking> {
   Map selectedVehicle;
   final vehicleService = VehicleService();
   String h;
-  List stateService;
- 
+  List stateService = [];
+  String date = 'Select Date';
+
   bool isTrue = false;
   final servicesService = ServicesService();
   List services;
+  DateTime _date = DateTime.now();
 
   @override
   void initState() {
@@ -29,16 +34,14 @@ class _ServiceBookingState extends State<ServiceBooking> {
   }
 
   void getServices() async {
-    //print('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
     try {
       var data = await servicesService.getServices('5f2f96be82e61e5274037800');
       setState(() {
         services = data;
       });
       stateServices(services);
-      //print(services);
     } catch (err) {
-      //print(err);
+      print(err);
     }
   }
 
@@ -48,9 +51,8 @@ class _ServiceBookingState extends State<ServiceBooking> {
       setState(() {
         vehicles = data;
       });
-      //print(vehicles);
     } catch (err) {
-      //print(err);
+      print(err);
     }
   }
 
@@ -72,16 +74,44 @@ class _ServiceBookingState extends State<ServiceBooking> {
     stateService = services;
   }
 
+  Future<Null> selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime.now().subtract(Duration(days: 0)),
+      lastDate: DateTime(2050),
+    );
+
+    if (picked != null && picked != _date) {
+      setState(() {
+        _date = picked;
+        print(_date.toString());
+        date = _date.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    //final List args = ModalRoute.of(context).settings.arguments;
-    //stateServices(args);
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Container(
+          child: Text('Online Service Booking'),
+          margin: EdgeInsets.only(right: 58.0),
+        )),
+      ),
       body: Container(
           child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           //select vehicle
-          Expanded(
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 35),
             child: DropdownButtonHideUnderline(
               child: ButtonTheme(
                 alignedDropdown: true,
@@ -109,21 +139,43 @@ class _ServiceBookingState extends State<ServiceBooking> {
               ),
             ),
           ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                'Select Services',
+                style: TextStyle(color: Colors.grey[600]),
+              )),
           Expanded(
             child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
               children: stateService.map((item) {
-                return new CheckboxListTile(
-                  title: new Text(item['servicename']),
-                  value: item['state'],
-                  onChanged: (bool value) {
-                    setState(() {
-                      item['state'] = value;
-                    });
-                  },
-                );
-              }).toList(),
+                    return new CheckboxListTile(
+                      title: new Text(item['servicename']),
+                      value: item['state'],
+                      onChanged: (bool value) {
+                        setState(() {
+                          item['state'] = value;
+                        });
+                      },
+                    );
+                  }).toList() ??
+                  [],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 50),
+            child: RaisedButton(
+              onPressed: () {
+                selectDate(context);
+              },
+              color: Colors.grey[100],
+              child: Text('$date'),
             ),
           )
+          //Text(widget.something),
         ],
       )),
     );
