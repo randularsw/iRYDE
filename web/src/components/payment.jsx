01fb99493fecd12ad9068ed78b10164d06cd2aa2
nawanjana <nkeshalya@gmail.com>
2020-09-03 14:29:45 +0530

    updated payment

--- web/src/components/payment.jsx
+++ web/src/components/payment.jsx
@@ -14,14 +14,12 @@ export default function Payment() {
       <Header />
       <Container className=" mt--9" fluid>
         {/* Table */}
-        <Col lg="10">
+        <Col lg="8">
           <Card
             className="bg-secondary shadow border-0 mb-7"
-            style={({ backgroundColor: "#f4f5f7" }, { marginLeft: "9.8rem" })}
+            style={({ backgroundColor: "#f4f5f7" }, { marginLeft: "20rem" })}
           >
-            <CardHeader className=" bg-transparent">
-              <h3 className=" mb-0">Add promotions</h3>
-            </CardHeader>
+            <CardHeader className=" bg-transparent"></CardHeader>
             <CardBody>
               <div style={{ minHeight: 400 }}>
                 {checkout === true ? (
@@ -47,69 +45,5 @@ export default function Payment() {
         </Col>
       </Container>
     </>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     {checkout === true ? (
-    //       <div className="payment-div">
-    //        <ReactPayPal />
-    //       </div>
-    //     ) : (
-    //       <div>
-    //         <h1>React-PayPal</h1>
-    //         <button
-    //           onClick={() => {
-    //             setCheckout(true);
-    //           }}
-    //           className="checkout-button"
-    //         >
-    //           Pay Subscription
-    //         </button>
-    //       </div>
-    //     )}
-    //   </header>
-    // </div>
   );
 }
-
-// class Payment extends Component {
-//   static contextType = UserContext;
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       servicename: "",
-//       description: "",
-//       upload: "",
-
-//       //items: [],
-//     };
-//   }
-
-//   render() {
-//     // const { items } = this.state;
-
-//     return (
-//       <>
-//         <Header />
-//         <Container className=" mt--9" fluid>
-//           {/* Table */}
-//           <Row>
-//             <div className=" col">
-//               <Card className=" shadow" style={{ backgroundColor: "#f4f5f7" }}>
-//                 <CardHeader className=" bg-transparent">
-//                   <h3 className=" mb-0">Add Services</h3>
-//                 </CardHeader>
-//                 <CardBody>
-//                   <div style={{ minHeight: 400 }}>{/* Page Content */}</div>
-//                 </CardBody>
-//               </Card>
-//             </div>
-//           </Row>
-//         </Container>
-//       </>
-//     );
-//   }
-// }
-
-//export default Payment;
