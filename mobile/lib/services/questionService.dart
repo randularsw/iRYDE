58e64b95eeae53e8c0cc5c05e173ae8a70c79e5d
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-09-05 10:57:09 +0530

    dependancies

---
+++ mobile/lib/services/questionService.dart
@@ -0,0 +1,13 @@
+// import 'dart:convert';
+
+// import 'package:http/http.dart' as http;
+
+// class QuestionService {
+//   Future<http.Response> createQuestion() async {
+//     return await http.post('http://localhost:4000/api/questions',
+//         body: jsonEncode(<String, String>{
+//           'title': title,
+//           'text': text,
+//         }));
+//   }
+// }
