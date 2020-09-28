5589255450e2737d7630e39f3a2bb14104c1a2d3
Randula Wickramasuriya <randularsw@gmail.com>
2020-09-28 13:09:53 +0530

    mobile notification screen

--- mobile/lib/screens/notifications/notificationPage.dart
+++ mobile/lib/screens/notifications/notificationPage.dart
@@ -9,6 +9,10 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  List notifications = [
+    {"title": "xHellos srth ert roih rh iosf hfd"},
+    {"title": "Hellos srth ert roih r"}
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,221 +22,73 @@ class _NotificationPageState extends State<NotificationPage> {
             child: Text('Notifications'),
             margin: EdgeInsets.only(right: 58.0),
           )),
-
-          // actions: <Widget>[
-          //   IconButton(
-          //     //alignment: Alignment.centerLeft,
-          //     //padding: EdgeInsets.only(right: 10.0),
-          //     splashColor: Colors.white12,
-          //     icon: Icon(
-          //       FontAwesomeIcons.backspace,
-          //       size: 20.0,
-          //     ),
-          //     onPressed: () {
-          //       // go back
-          //       Navigator.pop(context);
-          //     },
-          //   ),
-          // ],
         ),
         drawer: DrawerOption(),
         body: Container(
-          child: ListWheelScrollView(
-            itemExtent: 100.0,
-            // useMagnifier: true,
-            magnification: 1.5,
-            diameterRatio: 10.0,
-
-            children: <Widget>[
-              NotificationTile(),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 2',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
+          child: ListView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: notifications?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 80,
+                  //color: Colors.amber[colorCodes[index]],
+                  child: GestureDetector(
+                    onTap: () => print(55),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Container(
+                        // width: MediaQuery.of(context).size.width,
+                        // height: 50.0,
+                        color: Colors.grey[200],
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            notifications[index]["title"],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                // fontSize: 20.0,
+                                ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 3',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 4',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 5',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 6',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 7',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Notification 8',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+                );
+              }),
         ));
   }
 }
 
-class NotificationTile extends StatelessWidget {
-  const NotificationTile({
-    Key key,
-  }) : super(key: key);
+// class NotificationTile extends StatelessWidget {
+//   const NotificationTile({
+//     Key key,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 20.0,
-        right: 20.0,
-        bottom: 20.0,
-      ),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 20.0,
-        color: Colors.blue,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text(
-            'Notification 1',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.only(
+//         left: 20.0,
+//         right: 20.0,
+//         bottom: 20.0,
+//       ),
+//       child: Container(
+//         width: MediaQuery.of(context).size.width,
+//         height: 20.0,
+//         color: Colors.blue,
+//         child: Padding(
+//           padding: const EdgeInsets.all(10.0),
+//           child: Text(
+//             'Notification 1',
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               fontSize: 20.0,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
