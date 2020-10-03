550d787e96c66446ad9ef3529e9d5a754c5f1222
Randula Wickramasuriya <randularsw@gmail.com>
2020-10-03 13:24:17 +0530

    mobile - fix scrolling in login and register

--- mobile/lib/screens/auth/loginPage.dart
+++ mobile/lib/screens/auth/loginPage.dart
@@ -58,109 +58,111 @@ class _LoginPageState extends State<LoginPage> {
           child: Text('Login'),
         )),
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 6,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 50),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
-                            icon: const Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: const Icon(FontAwesomeIcons.solidEnvelope),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 6,
+              child: Form(
+                key: _formKey,
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              icon: const Padding(
+                                padding: const EdgeInsets.only(top: 15.0),
+                                child:
+                                    const Icon(FontAwesomeIcons.solidEnvelope),
+                              ),
                             ),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              if (emailError == "Email doesn't exist") {
+                                String err = emailError;
+                                emailError = null;
+                                return err;
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              email = value;
+                            },
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your email';
-                            }
-                            if (emailError == "Email doesn't exist") {
-                              String err = emailError;
-                              emailError = null;
-                              return err;
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            email = value;
-                          },
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 50),
-                        child: TextFormField(
-                          obscureText: !visiblePassword,
-                          decoration: const InputDecoration(
-                            labelText: 'Password',
-                            icon: const Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: const Icon(FontAwesomeIcons.key),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          child: TextFormField(
+                            obscureText: !visiblePassword,
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              icon: const Padding(
+                                padding: const EdgeInsets.only(top: 15.0),
+                                child: const Icon(FontAwesomeIcons.key),
+                              ),
                             ),
+                            keyboardType: TextInputType.visiblePassword,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              if (passwordError == "Invalid password") {
+                                String err = passwordError;
+                                passwordError = null;
+                                return err;
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              password = value;
+                            },
                           ),
-                          keyboardType: TextInputType.visiblePassword,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your password';
-                            }
-                            if (passwordError == "Invalid password") {
-                              String err = passwordError;
-                              passwordError = null;
-                              return err;
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            password = value;
-                          },
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      RaisedButton(
-                        color: Color(0XFF172b4d),
-                        textColor: Colors.white,
-                        onPressed: () {
-                          loginUser();
-                        },
-                        child: Text('Login'),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 20,
+                        ),
+                        RaisedButton(
+                          color: Color(0XFF172b4d),
+                          textColor: Colors.white,
+                          onPressed: () {
+                            loginUser();
+                          },
+                          child: Text('Login'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Still not a user? '),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, RegisterPage.id);
-                      },
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.blue),
-                      ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Still not a user? '),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, RegisterPage.id);
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.blue),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
