ae1c1246392aab5eaa872124c2b233a42178037e
erandidilshani <36620007+erandidilshani@users.noreply.github.com>
2020-10-05 16:21:26 +0530

    Update vehicleTypes.js

--- backend/routes/vehicleTypes.js
+++ backend/routes/vehicleTypes.js
@@ -1,32 +1,76 @@
 const router = require('express').Router();
-let VehicleType=require('../models/vehicleType');
 const vehicleType = require('../models/vehicleType');
 
+// add vehicle brand
+router.post('/', async (req,res) =>{
+    console.log(req.body);
+    const newVehicleType = new vehicleType({
+        brand:req.body.brand,
+        
+    })
+    try {
+        const saved = await newVehicleType.save();
+        res.json(saved);    
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+//add vehicle model
+router.put('/add/:id', async (req,res) =>{
+    console.log(req.body);
+    
+    try {
+        const brand = await vehicleType.findById(req.params.id);
+        console.log(brand);
+        // const newVehicleType = new vehicleType({
+    
+        //     model:req.body.model,
+        //     type:req.body.type, 
+            
+        // });
+    
+        brand.models.push(req.body);
+        console.log(888,brand);
+        saved = await brand.save();
+        res.json(saved); 
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+//get vehicle brand
 router.route('/').get((req,res)=>{
-    VehicleType.find()
+    vehicleType.find()
     .then(vehicleTypes=> res.json(vehicleTypes))
     .catch(err => res.status(400).json('Error:'+err));
 
 });
 
-router.route('/add').post((req,res)=>{
-    const vehicleType = req.body.vehicleType;
-    const newVehicleType = new VehicleType({vehicleType});
+//get vehicle model & type
+router.route('/model').get((req,res)=>{
+    vehicleModel.find()
+    .then(vehicleModels=> res.json(vehicleModels))
+    .catch(err => res.status(400).json('Error:'+err));
 
-    newVehicleType.save()
-    .then(()=>res.json('Vehicle type added!'))
-    .catch(err=> res.status(400).json('Error:'+err));
 });
 
-router.route('/:id').get((req,res)=>{
-    VehicleType.findById(req.params.id)
-    .then(vehicleType=> res.json(vehicleType))
+// router.route('/:id').get((req,res)=>{
+//     vehicleType.findById(req.params.id)
+//     .then(vehicleType=> res.json(vehicleType))
+//     .catch(err =>res.status(400).json('Error:'+err));
+// });
+
+//delete vehicle brand
+router.route('/brand/:id').delete((req,res)=>{
+    vehicleType.findByIdAndDelete(req.params.id)
+    .then(()=> res.json('Vehicle Brand Deleted'))
     .catch(err =>res.status(400).json('Error:'+err));
 });
 
-router.route('/:id').delete((req,res)=>{
-    VehicleType.findByIdAndDelete(req.params.id)
-    .then(()=> res.json('Vehicle Type Deleted'))
+router.route('/model/:id').delete((req,res)=>{
+    vehicleType.findByIdAndDelete(req.params.id)
+    .then(()=> res.json('Vehicle Model Deleted'))
     .catch(err =>res.status(400).json('Error:'+err));
 });
 
