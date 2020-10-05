f28f198d02b4909011e362fab1d0fec4c8d8b302
yashanmick <yashanthilakasiri@gmail.com>
2020-10-05 16:22:34 +0530

    updataQuest, deleteQuest, likeQuest, userHandle

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
     const newQuestion = new Question({
         title: req.body.title,
         text: req.body.text,
-        // ownerId: req.body.ownerId,
+        userHandle: req.body.userHandle,
     });
     try {
         const saved = await newQuestion.save();
@@ -19,9 +19,25 @@ router.post('/', async (req, res) => {
 
 //get specific question details
 router.get('/:id', async (req, res) => {
-    console.log('vvvvvvvvvvvvvvvvv', req.params.id);
+    // console.log('vvvvvvvvvvvvvvvvv', req.params.id);
     try {
         const question = await Question.findById({ _id: req.params.id });
+        console.log(question);
+        if (question._id) {
+            res.json(question)
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Message not found!' });
+    }
+});
+
+//get all question details
+router.get('/', async (req, res) => {
+    var mysort = { createdAt: -1 };
+    // console.log('vvvvvvvvvvvvvvvvv',req.body);
+    try {
+        const question = await Question.find().sort(mysort);
         console.log(question);
         res.json(question)
     } catch (error) {
@@ -29,17 +45,46 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-//get specific question details
-router.get('/', async (req, res) => {
-    // console.log('vvvvvvvvvvvvvvvvv',req.body);
+//update an question
+router.put("/:id", async (req, res) => {
     try {
-        const question = await Question.find();
-        console.log(question);
-        res.json(question)
+        const updateQuestion = await Question.findById({ _id: req.params.id });
+        console.log(req.params);
+        if (updateQuestion._id) {
+            updateQuestion.title = req.body.title;
+            updateQuestion.text = req.body.text;
+        }
+        const saved = await updateQuestion.save();
+        res.json(saved);
     } catch (error) {
         res.json({ message: error });
     }
 });
+
+//Delete question
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedQuestion = await Question.findByIdAndDelete({ _id: req.params.id });
+        res.json(deleted);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+//Like a question
+router.put('/:id/like', async (req, res) => {
+    // const userHandle = req.body.userHandle;
+    const question = await Question.updateOne({ _id: req.params.id });
+    console.log(req.params);
+    if (question._id) {
+        question.likeCount = question.likeCount + 1;
+        // question.likes.push({ userHandle: userHandle });
+        const likedQuestion = await question.save();
+        res.json(likedQuestion);
+    } else {
+        res.status(500).json({ error: 'Message not found!' });
+    }
+})
 
 
 
