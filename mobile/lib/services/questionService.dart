53b78c1512f053e0d236c3cc58a7e31e88ade059
yashanmick <yashanthilakasiri@gmail.com>
2020-10-05 16:23:54 +0530

    getAllQuest, getSpecificQuest, updateQuest, deleteQuest

--- mobile/lib/services/questionService.dart
+++ mobile/lib/services/questionService.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
+import 'package:iRYDE/core/globals.dart';
 import 'dart:convert';
 
 class QuestionService {
+  //Add new question
   Future<Map> addQuestion(Map question) async {
     print(question);
     try {
-      final res = await http.post('http://192.168.1.5:4000/api/questions',
+      final res = await http.post('$baseUrl/api/questions',
           headers: null, body: question);
 
       Map data = jsonDecode(res.body);
@@ -16,9 +18,32 @@ class QuestionService {
     }
   }
 
+  //Get all questions
   Future<List> getAllQuestions() async {
-    final res = await http.get('http://192.168.1.5:4000/api/questions');
+    final res = await http.get('$baseUrl/api/questions');
     List data = jsonDecode(res.body);
+    return data;
+  }
+
+  //Get an specific question
+  Future<List> getSpecificQuestion() async {
+    final res = await http.get('$baseUrl/api/questions/:id');
+    List data = jsonDecode(res.body);
+    return data;
+  }
+
+  //Update question
+  Future<List> updateQuestion() async {
+    final res = await http.patch('$baseUrl/api/questions/:id');
+    List data = jsonDecode(res.body);
+    return data;
+  }
+
+  //delete an question
+  Future<Map> deleteQuestion(id) async {
+    final res = await http.delete('$baseUrl/api/questions/:id');
+    Map data = jsonDecode(res.body);
+    // print(data);
     return data;
   }
 }
