e86a25203706371e87e2970c5bb62196c283213d
yashanmick <yashanthilakasiri@gmail.com>
2020-10-05 16:19:56 +0530

    question schema updated

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const QuestionSchema = mongoose.Schema(
   {
+    userHandle: {
+      type: String,
+      // required: true,
+    },
     title: {
       type: String,
       required: true,
@@ -10,14 +14,43 @@ const QuestionSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    likeCount: {
+      type: Number
+    },
+
+    commentCount: {
+      type: Number
+    },
+    likes: {
+      type: [{
+        userHandle: String,
+      }]
+    },
     // ownerId: {
     //   type: String,
     //   required: true,
     // },
 
   },
+  // {
+  //   comments: [
+  //     {
+  //       userHandle: 'user',
+  //       questionId: 'kdjsfgdksuufhgkdsufky',
+  //       body: 'nice one mate!',
+  //       createdAt: '2019-03-15T10:59:52.798Z',
+  //       userImage: 'image/dsfsdkfghskdfgs/dgfdhfgdh'
+  //     }
+  //   ],
+  // },
+  // {
+  //   timestamps: true,
+  // },
   {
-    timestamps: true,
+    createdAt: {
+      type: Date,
+      required: true,
+    },
   }
 );
 
