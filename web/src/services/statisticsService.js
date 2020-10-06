14d5f10cc8da1626d6023858508e1adc265cfb65
erandidilshani <36620007+erandidilshani@users.noreply.github.com>
2020-10-06 18:33:53 +0530

    Update statisticsService.js

--- web/src/services/statisticsService.js
+++ web/src/services/statisticsService.js
@@ -12,7 +12,16 @@ export function getCountVo(){
 export function getCountBookings(){
     return axios.get('http://localhost:4000/count/');
 }
+export function getAllBookings(){
+    return axios.get('http://localhost:4000/bookings/all-bookings');
+}
 
+export function getSB(){
+    return axios.get('http://localhost:4000/api/users/sp');
+}
+export function getVO(){
+    return axios.get('http://localhost:4000/api/users/vo');
+}
 // export function getCountBookings(){
 //     return axios.get('http://localhost:4000/counts/bookings');
 // }
